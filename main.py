89573f9fcde48fe20244593f2f8e7d9698662973
Arthur Ufongene <f007hcb@lisplab3.thayer.dartmouth.edu>
2025-08-10 19:55:34 -0400

    Added click to open conversation agent flow

--- main.py
+++ main.py
@@ -3,13 +3,41 @@ import agent
 import google.generativeai as genai
 import json
 import threading
+from pynput import mouse
 
-prompts = []
-gotten = threading.Condition()
+new_emails = []
+end = False
+notifier = threading.Condition()
 
-def insert_prompts(list_of_emails, prompts):
-    with gotten:
-        prompts.extend(list_of_emails)
-        gotten.notify()
+def insert_emails(list_of_emails):
+    global new_emails
+    with notifier:
+        new_emails.extend(list_of_emails)
+        notifier.notify()
 
-agent.start_conversation_agent()
+def mouse_listener():
+    def on_click(x, y, button, pressed):
+        if pressed:
+            if button == mouse.Button.right:
+                end = True
+
+            with notifier:
+                notifier.notify()
+
+    with mouse.Listener(on_click=on_click) as listener:
+        listener.join()
+
+agent.start_backlog_agent(gmail_tools.get_backlogged_emails())
+
+gmail_tools.start_email_checking(utils.creds, insert_emails)
+
+while True:
+    with notifier:
+        notifier.wait()
+        
+    if end: break
+
+    if new_emails:
+        agent.start_new_email_agent(new_emails.pop(0))
+    else:
+        agent.start_conversation_agent()
